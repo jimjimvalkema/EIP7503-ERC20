7957d8e11305ad00163e7cf557d2d20d25dac55c
jimjimvalkema <jjvalkema@protonmail.com>
2025-10-30 16:19:07 -0300

    added script to compile noir and generate solidity verifier

--- scripts/replaceLine.ts
+++ scripts/replaceLine.ts
@@ -1,17 +1,19 @@
 import { ArgumentParser } from 'argparse';
 import fs from "fs/promises";
 
+export interface lineReplacement {
+    original: string;
+    replacement: string;
+}
 
-async function lineReplacer(filePath, lineReplacements) {
+export async function lineReplacer(filePath:string, lineReplacements: lineReplacement[]) : Promise<void> {
     const file = await fs.open(filePath, "r")
     let newFile = ""
 
     for await (const line of file.readLines()) {
-        //if (line.startsWith("contract HonkVerifier is IVerifier")) {  
         const replacement = lineReplacements.find((replacement) => line.startsWith(replacement.original))
         if (replacement) {
             newFile += replacement.replacement + "\n"
-            console.log({ replacement })
         } else {
             newFile += line + "\n"
         }
@@ -20,16 +22,11 @@ async function lineReplacer(filePath, lineReplacements) {
     await fs.writeFile(filePath, newFile);
 }
 
-
-
-
 async function main() {
     const parser = new ArgumentParser({
-        description: 'Argparse example',
-        usage: `quick lil script to replace 1 line`
+        description: 'quick lil script to replace 1 line',
+        usage: `yarn ts-node scripts_dev_op/replaceLine.ts --file contracts/evm/WithdrawVerifier.sol --remove "contract UltraVerifier is BaseUltraVerifier {" --replace "contract WithdrawVerifier is BaseUltraVerifier {"`
     });
-    console.log(parser)
-
     parser.add_argument('-f', '--file', { help: 'file to read', required: true, type: 'str' });
     parser.add_argument('-r', '--remove', { help: 'specify what line to replace', required: true, type: 'str' });
     parser.add_argument('-p', '--replace', { help: 'specify what to replace it with', required: true, type: 'str' });
@@ -42,8 +39,5 @@ async function main() {
         }
     ]
     await lineReplacer(args.file, lineReplacements)
-
-
 }
-
 await main()
