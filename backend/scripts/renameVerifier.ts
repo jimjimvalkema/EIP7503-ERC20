841eec617154d422a5c8b386d72ac6c6612020b3
jimjimvalkema <jjvalkema@protonmail.com>
2025-11-22 19:45:55 -0300

    cleanup, fixes, setup yarn script to generate 1n verifier

--- backend/scripts/renameVerifier.ts
+++ backend/scripts/renameVerifier.ts
@@ -1,18 +1,29 @@
-import { PrivateTransferVerifierContractName } from "../src/constants.js"
+import { ArgumentParser } from "argparse";
 import { lineReplacer } from "./replaceLine.js"
 
-function getLine(name:string) {
+const originalName = "HonkVerifier"
+
+
+function getLine(name: string) {
     return `contract ${name} is BaseZKHonkVerifier(N, LOG_N, VK_HASH, NUMBER_OF_PUBLIC_INPUTS) {`
 }
 
-const file = "contracts/PrivateTransferVerifier.sol"
-const originalName = "HonkVerifier"
-const newName = PrivateTransferVerifierContractName
+const parser = new ArgumentParser({
+    description: 'quick lil script to replace 1 line',
+    usage: `yarn tsx scripts_dev_op/replaceLine.ts --file contracts/evm/WithdrawVerifier.sol --newName privateTransferVerifier1n`
+});
+parser.add_argument('-f', '--file', { help: 'file to read', required: true, type: 'str' });
+parser.add_argument('-r', '--newName', { help: 'specify what new name the verifier needs', required: true, type: 'str' });
+const args = parser.parse_args()
+
+const file = args.file
+const newName = args.newName
+
 
 const lineReplacements = [
     {
-        "original"      :getLine(originalName),
-        "replacement"   :getLine(newName)
+        "original": getLine(originalName),
+        "replacement": getLine(newName)
     }
 ]
 await lineReplacer(file, lineReplacements)
