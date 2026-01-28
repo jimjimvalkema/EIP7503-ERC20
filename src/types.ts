bfcfe62f43f42b66adedb14cd08875a2dcab692b
jimjimvalkema <jjvalkema@protonmail.com>
2026-01-28 16:09:20 +0100

    cleaned up types so the match the new circuit. Removed Unformatted types. Instead all types are ready for noir and can be serialized as JSON strings for the relayer

--- src/types.ts
+++ src/types.ts
@@ -3,9 +3,72 @@ import { WormholeToken$Type } from "../artifacts/contracts/WormholeToken.sol/art
 import { SignMessageReturnType } from "viem/accounts";
 import { InputMap } from "@noir-lang/noir_js";
 import { ProofData } from "@aztec/bb.js";
-import { FeeData, FormattedBurnAddressProofDataPublic, UnformattedProofInputsPublic } from "./proofInputsTypes.js";
 
 export type WormholeToken = GetContractReturnType<WormholeToken$Type["abi"], Required<{ public?: PublicClient; wallet?: WalletClient; }>>
+
+// we could use import type { FixedLengthArray } from 'type-fest';
+// but for now i just do this
+export type u8sAsHexArrLen32 = Hex[]
+export type u8sAsHexArrLen64 = Hex[]
+export type u32AsHex = Hex
+export type u1AsHexArr = Hex[]
+
+export interface SignatureData extends InputMap {
+    /** Must be exactly 32 bytes */
+    public_key_x: u8sAsHexArrLen32;
+    /** Must be exactly 32 bytes */
+    public_key_y: u8sAsHexArrLen32;
+    /** Must be exactly 64 bytes */
+    signature: u8sAsHexArrLen64;
+}
+
+export interface MerkleData extends InputMap{
+    depth: u32AsHex,
+    // TODO maybe we can save on memory computing indices on the spot instead?
+    indices: u1AsHexArr,
+    siblings: Hex[],
+}
+
+
+export interface BurnDataPublic extends InputMap {
+    account_note_hash: Hex,       
+    account_note_nullifier: Hex,                   
+}
+
+export interface BurnDataPrivate extends InputMap {                
+    //-----very privacy sensitive data -----
+    total_received: Hex,              
+    prev_total_spent: Hex,                           
+    prev_account_nonce: Hex,               
+    prev_account_note_merkle: MerkleData,
+    total_received_merkle: MerkleData,
+    amount: Hex,
+    blinding_pow: Hex,
+}
+
+export interface PublicProofInputs extends InputMap {
+    root: Hex,
+    amount: Hex,
+    signature_hash: u8sAsHexArrLen32,
+    burn_data_public: BurnDataPublic[],
+}
+
+export interface PrivateProofInputs extends InputMap  {
+    signature_data: SignatureData,      
+    viewing_key: Hex,
+    burn_data_private: BurnDataPrivate[],
+    amount_burn_addresses:u32AsHex 
+}
+
+interface ProofInputs extends PublicProofInputs,PrivateProofInputs, InputMap {}
+
+export interface ProofInputs1n extends ProofInputs {
+    amount_burn_addresses: '0x0' | '0x1';
+}
+
+export interface ProofInputs4n extends ProofInputs {
+    amount_burn_addresses: '0x0' | '0x1' | '0x2' | '0x3' | '0x4';
+}
 
 export interface UnsyncedPrivateWallet {
     pubKey: { x: Hex, y: Hex };
@@ -27,28 +90,25 @@ export interface SyncedPrivateWallet extends UnsyncedPrivateWallet {
     spendableBalance: bigint;
 }
 
-export interface UnformattedPublicProofInputsHex {
-    
-}
- 
 
 export interface RelayerInputs {
-    pubInputs: FormattedBurnAddressProofDataPublic[];
-    feeData:FeeData
-    zkProof: ProofData;
-    claimedAmounts: bigint[]
-}
-
-export interface RelayerInputsHex {
-    pubInputs: UnformattedPublicProofInputsHex;
+    pubInputs: PublicProofInputs;
     zkProof: {
         proof:Hex,
         publicInputs:Hex[]
     };
 }
 
-export interface SignatureData {
-    publicKeyX: Hex,
-    publicKeyY: Hex,
-    signature: Hex
+export interface FeeData {
+    relayerAddress: Address,
+    priorityFee: Hex,
+    conversionRate: Hex,
+    maxFee: Hex,
+    feeToken: Address,
+}
+
+export interface SignatureHashPreImg {
+    recipientAddress: Address, 
+    amount: Hex ,
+    callData: Hex,
 }
